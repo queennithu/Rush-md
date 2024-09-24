fc49dab6fecce18668da5e2fa22b4d37e2bc8476
dragen md <nimsaravihaga569@gmail.com>
2024-09-24 07:27:41 +0530

    Update rushmdyt.js

--- rush/plugin/rushmdyt.js
+++ rush/plugin/rushmdyt.js
@@ -1,21 +1,40 @@
-import config from '../../config.cjs';
+const report = async (m, gss) => {
+  const reportedMessages = {};
+  const devlopernumber = '911111111';
+const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
 
-const ownerContact = async (m, gss) => {
-    const ownernumber = config.OWNER_NUMBER;
-    const prefix = config.PREFIX;
-const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-const text = m.body.slice(prefix.length + cmd.length).trim();
+  const validCommands = ['ping']; 
+  
+  if (validCommands.includes(cmd)) {
+    
+  if (!text) return m.reply(`pong 10✨`);
 
-    if (cmd === 'owner') {
-        try {
-            await gss.sendContact(m.from, [ownernumber], m);
-            await m.React("✅");
-        } catch (error) {
-            console.error('Error sending owner contact:', error);
-            m.reply('Error sending owner contact.');
-            await m.React("❌");
-        }
+    const messageId = m.key.id;
+
+    if (reportedMessages[messageId]) {
+        return m.reply("This song has already been forwarded to the owner. Please wait for a response.");
     }
+
+    reportedMessages[messageId] = true;
+
+    const textt = `*| alive |*`;
+    const teks1 = `\n\n*User*: @${m.sender.split("@")[0]}\n*alive*: ${text}`;
+    const teks2 = `\n\n*Hi ${m.pushName}, your song has been forwarded to my Owners.*\n*Please wait...*`;
+
+    // Send the message to the first owner in the `owner` array
+    gss.sendMessage(devlopernumber + "@s.whatsapp.net", {
+        text: textt + teks1,
+        mentions: [m.sender],
+    }, {
+        quoted: m,
+    });
+
+    // don,t edite this message
+    m.reply("suvi tech.");
+   }
 };
 
-export default ownerContact;
+export default report;
