9234ad0b3cb01ccc566ecb33e4f7f5aedc3c52fa
dragen md <nimsaravihaga569@gmail.com>
2024-09-25 19:22:26 +0530

    Create Rushmdalived.js

---
+++ rush/plugin/Rushmdalived.js
@@ -0,0 +1,201 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+import config from '../../config.cjs';
+
+const alive = async (m, Matrix) => {
+  const uptimeSeconds = process.uptime();
+  const days = Math.floor(uptimeSeconds / (24 * 3600));
+  const hours = Math.floor((uptimeSeconds % (24 * 3600)) / 3600);
+  const minutes = Math.floor((uptimeSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeSeconds % 60);
+  
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    if (['alive', 'uptime', 'runtime'].includes(cmd)) {
+
+  const uptimeMessage = `*RUSH MD SETTING*
+___`;
+
+  const buttons = [
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "AUTO STATUS ON",
+            id: `${prefix}autosview ON`
+          })
+        },
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "AUTO STATUS DELETE",
+            id: `${prefix}autosview off`
+          })
+        }
+        ];
+
+  const msg = generateWAMessageFromContent(m.from, {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: uptimeMessage
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "𝚛𝚞𝚜𝚑 𝚖𝚍"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            gifPlayback: true,
+            subtitle: "",
+            hasMediaAttachment: false 
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons
+          }),
+          contextInfo: {
+                  quotedMessage: m.message,
+                  mentionedJid: [m.sender], 
+                  forwardingScore: 999,
+                  isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                  newsletterJid: '11111@newsletter',
+                  newsletterName: "rush",
+                  serverMessageId: 143
+                }
+              }
+        }),
+      },
+    },
+  }, {});
+
+  await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+    messageId: msg.key.id
+  });
+    }
+};
+
+export default alive;
